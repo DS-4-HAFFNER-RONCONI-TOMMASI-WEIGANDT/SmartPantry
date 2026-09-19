9df06b6282d04ed657d36d10911514d641da10e7
MartinRonconi7 <ronconimartin4@gmail.com>
2026-09-19 20:42:02 -0300

    feat(application-contracts): add Product DTOs and IProductAppService (#12)

---
+++ src/TP04.Application.Contracts/Products/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace TP04.Products
+{
+    public class ProductDto : AuditedEntityDto<Guid>
+    {
+        public string Barcode { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Brand { get; set; }
+    }
+}
